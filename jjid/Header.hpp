4f99af506bf87a87b0f0f6ede4a9930900d1e412
lewis941001 <lewis1004@naver.com>
2022-05-09 13:16:11 +0900

    header getContent

--- jjid/Header.hpp
+++ jjid/Header.hpp
@@ -2,14 +2,17 @@
 # define HEADER_HPP
 
 #include "uniHeader.hpp"
+#include <list>
 
 class Header
 {
 	protected:
-		std::string date;
-		std::string connection;
-		int contentLength;
+		std::map <std::string, std::string > content;
 	public:
+		std::map <std::string, std::string >& getContent()
+		{
+			return (content);
+		}
 		
 };
 
